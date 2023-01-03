ea4688ead4978c25564c0bedbd96b01924859c73
kage08 <harshavardhan864.hk@gmail.com>
2023-01-03 08:56:03 -0500

    Run Files

--- runs/ar_wt_shift.py
+++ runs/ar_wt_shift.py
@@ -1,25 +1,27 @@
 import subprocess
-from runs.consts import states as states
+from runs.consts import imp_states as states
 
 for curr in range(11):
-    for decoder in ["gru"]:
+    for wt in [50, 500]:
         for region in states:
             print(f"Training {region}...")
             subprocess.run(
                 [
                     "python",
-                    "train_ili_max_ar.py",
+                    "train_ili_max_weight.py",
                     "-y",
                     str(2022),
                     "-a",
                     "trans",
                     "-d",
-                    decoder,
+                    "gru",
                     "-r",
                     region,
                     "-e",
                     str(5500),
                     "-c",
                     str(curr),
+                    "--weight",
+                    str(wt),
                 ]
             )
